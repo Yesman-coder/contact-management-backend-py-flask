954fa5eacd7aacc907f00811a14005986dea3cfa
Yesman-coder <yesmanshiva@gmail.com>
2020-09-10 02:49:12 +0000

    :green_book: some endpoints working as expected some more to add

--- src/main.py
+++ src/main.py
@@ -9,7 +9,7 @@ from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
 from models import db, User
-#from models import Person
+from models import Contact
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -19,30 +19,6 @@ MIGRATE = Migrate(app, db)
 db.init_app(app)
 CORS(app)
 setup_admin(app)
-
-todos = [
-        {"label": "Do a little dance", "done": False},
-        {"label": "Make a little love", "done": False}
-    ]
-
-@app.route('/todos', methods=['GET'])
-def hello_world():
-    request_body = jsonify(todos)
-    return request_body
-
-@app.route('/todos', methods=['POST'])
-def add_new_todo():
-    # request_body = request.data
-    decoded_object = json.loads(request.data)
-    todos.append(decoded_object)
-    print("Incoming request with the following body" , decoded_object)
-    return jsonify(todos)
-
-@app.route('/todos/<int:position>', methods=['DELETE'])
-def delete_todo(position):
-    todos.pop(position)
-    print("This is the item to delete:", position)
-    return jsonify(todos)
 
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
@@ -54,14 +30,63 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+@app.route('/contact/all', methods=['GET'])
+def get_contacts():
+    # Select all entries from Contact table with flask_sqlalchemy
+    # Put them inside a list variable
+    contacts = Contact.query.all()
+    # Check if theres any parameters in the URL to filter the list
+    name = request.args.get("full_name")
+    if name is not None:
+        filtered_contacts = filter(lambda contact : full_name in contact.full_name, contacts)
+        name = request.args.get("name")
+    else:
+        filtered_contacts = contacts
+    # Serialize every object of the list to make a dictionary
+    serialized_contacts = list(map(lambda contact: contact.serialize(), filtered_contacts))
+    print(serialized_contacts)
+    # return serialized lis and 200-OK
+    return jsonify(serialized_contacts), 200   
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
 
-    return jsonify(response_body), 200
+@app.route('/contact', methods=['POST'])
+def add_contact():
+    contact_info = request.json
+
+    new_contact = Contact.create(
+        contact_info['full_name'],
+        contact_info['email'],
+        contact_info['phone'],
+        contact_info['address']
+        # contact_info['groups']
+    )
+    
+    db.session.add(new_contact)
+    try:
+        db.session.commit()
+        # serialize contact and return 201
+        return f"{contact_info['full_name']} added succesfully", 201
+    except Exception as error:
+        db.session.rollback()
+        print(f"{error.args} {type(error)}")
+        # return error
+        return jsonify({
+            "resultado": f"{error.args}"
+        }), 500
+
+@app.route('/contact/<int:contact_id>', methods=['GET'])
+def get_single_contact(contact_id):
+    contact = [contact for contact in contacts if contact['id'] == contact_id]
+    return jsonify(contact)
+
+# @app.route('/user', methods=['GET'])
+# def handle_hello():
+
+#     response_body = {
+#         "msg": "Hello, this is your GET /user response "
+#     }
+
+#     return jsonify(response_body), 200
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+import json
 
 db = SQLAlchemy()
 
@@ -17,3 +18,69 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class Contact(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    full_name = db.Column(db.String(80))
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    address = db.Column(db.String(120), nullable=False)
+    phone = db.Column(db.String(20), unique=True, nullable=False) 
+
+    # subscriptions = db.relationship("Subscription", backref="contact")
+
+    def __init__(self, full_name, email, address, phone):
+        """creates an instance of this class"""
+        self.full_name = full_name
+        self.email = email
+        self.address = address
+        self.phone = phone
+
+    def serialize(self):
+        """returns a dictionary with data from the object"""
+        return{
+            "full name" : self.full_name,
+            "email" : self.email,
+            "address" : self.address,
+            "phone" : self.phone
+            # "groups" : [subscriptions.serialize() for subscription in self.subscriptions]
+        }
+    
+    @classmethod
+    def create(cls, full_name, email, address, phone):
+        """
+            normaliza insumos nombre y apellido,
+            crea un objeto de la clase Contact con
+            esos insumos y devuelve la instancia creada.
+        """
+        new_contact = cls(
+            full_name,
+            email,
+            address,
+            phone
+        )
+        return new_contact
+
+class Subscription(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    contact_id = db.Column(db.Integer, db.ForeignKey('contact.id'))
+    group_id = db.Column(db.Integer, db.ForeignKey('group.id'))
+
+    def __init__(self, contact_id,  group_id):
+        """creates an instance of this class"""
+        self.contact_id = contact_id
+        self.group_id = group_id
+
+    def serialize(self):
+        """returns a dictionary with data from the object"""
+        return {
+            "id" : self.id,
+            "contact_id" : self.contact_id,
+            "group_id" : self.groupd_id,
+            "name" : self.contact.full_name
+        }
+
+class Group(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    group_name = db.Column(db.String(20), unique=True, nullable=False)
+
+    subscriptions = db.relationship("Subscription", backref="groups")
